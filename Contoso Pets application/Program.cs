003a71468d5ab6f7feef04e4be3041faa73a3d85
Yahia Hamed <yahiamh309090@outlook.com>
2025-07-18 02:36:45 +0300

    Add project files.

---
+++ Contoso Pets application/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Contoso_Pets_application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
